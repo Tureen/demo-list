c293ae257a3d2d3037470b7ef1630b83bf8f9f73
tulane <dpzhuming@szeastroc.com>
2020-01-01 23:01:34 +0800

    Added 杂

---
+++ demo-nio/src/main/java/com/tulane/nio/client/NioClient2.java
@@ -0,0 +1,52 @@
+package com.tulane.nio.client;
+
+import com.tulane.nio.base.ClientNioSocket;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.channels.SocketChannel;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
+/**
+ * NIO客户端
+ */
+public class NioClient2 {
+
+    /**
+     * 启动
+     * 1. 连接服务器端
+     * 2. 接收服务器端响应
+     * 3. 向服务器端发送数据
+     */
+    public void start() throws IOException {
+        SocketChannel socketChannel = SocketChannel.open(new InetSocketAddress("127.0.0.1", 8000));
+
+        readMessageFromServer(socketChannel);
+        sendMessageToServer(socketChannel);
+    }
+
+    private void readMessageFromServer(SocketChannel socketChannel) {
+        new Thread(() -> {
+            try {
+                new ClientNioSocket(socketChannel).start();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }).start();
+    }
+
+    private void sendMessageToServer(SocketChannel socketChannel) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextLine()){
+            String request = scanner.nextLine();
+            if(request != null && request.length() > 0){
+                socketChannel.write(StandardCharsets.UTF_8.encode(request));
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        new NioClient2().start();
+    }
+}
